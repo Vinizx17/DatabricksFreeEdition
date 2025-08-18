77fb6841b818e049aa277de963dacc7057bf049b
Vinizx17 <Vinizx17>
2025-08-18 16:10:26 +0000

    Add SQL Tasks

--- Pipeline/job.py
+++ Pipeline/job.py
@@ -16,6 +16,76 @@ Spark_Job = Job.from_dict(
                 },
                 "environment_key": "Default",
             },
+            {
+                "task_key": "SQL_estado_civil",
+                "depends_on": [
+                    {
+                        "task_key": "Spark_job",
+                    },
+                ],
+                "sql_task": {
+                    "query": {
+                        "query_id": "0e4e4f78-5bfd-4012-9fb4-26db84e52d08",
+                    },
+                    "warehouse_id": "3406b93f719bc4ca",
+                },
+            },
+            {
+                "task_key": "SQL_hora_extra",
+                "depends_on": [
+                    {
+                        "task_key": "Spark_job",
+                    },
+                ],
+                "sql_task": {
+                    "query": {
+                        "query_id": "baf24ca0-9684-444c-9883-e133c74311b5",
+                    },
+                    "warehouse_id": "3406b93f719bc4ca",
+                },
+            },
+            {
+                "task_key": "SQL_salario_departamento",
+                "depends_on": [
+                    {
+                        "task_key": "Spark_job",
+                    },
+                ],
+                "sql_task": {
+                    "query": {
+                        "query_id": "6803f010-92f0-452b-8eb9-f8897f537c3c",
+                    },
+                    "warehouse_id": "3406b93f719bc4ca",
+                },
+            },
+            {
+                "task_key": "SQL_satisfacao",
+                "depends_on": [
+                    {
+                        "task_key": "Spark_job",
+                    },
+                ],
+                "sql_task": {
+                    "query": {
+                        "query_id": "de68d569-1702-4a6d-b53a-c48f4ad8a8d0",
+                    },
+                    "warehouse_id": "3406b93f719bc4ca",
+                },
+            },
+            {
+                "task_key": "SQL_tempo_empresa",
+                "depends_on": [
+                    {
+                        "task_key": "Spark_job",
+                    },
+                ],
+                "sql_task": {
+                    "query": {
+                        "query_id": "0c5c304f-5b05-45f0-9260-ba95427447cb",
+                    },
+                    "warehouse_id": "3406b93f719bc4ca",
+                },
+            },
         ],
         "queue": {
             "enabled": True,
